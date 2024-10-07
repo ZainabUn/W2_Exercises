c88c4eceff2488d67961280ae2a062a4e7b82476
Zainab <zhaseeba1@appdev.yearup.org>
2024-10-07 11:30:51 -0500

    exercise 4.a Inserting rows

---
+++ 4A_restaurant_insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO Employees(EmployeeName)
+VALUES ('Zainab Haseeba')
+
+INSERT INTO menu  VALUES (1, 'Pizza');
+
+INSERT INTO Supplies
+VALUES (1, 'chair')
+
+INSERT INTO orders
+VALUES  (1,1,10);
